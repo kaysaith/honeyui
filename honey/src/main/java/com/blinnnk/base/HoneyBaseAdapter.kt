ad64e55a7ee8c8fee4e9e26e60a24f96f4efc165
Saith Kay <kaysaith@sina.com>
2017-11-21 00:45:11 +0800

    add base adapter

---
+++ honey/src/main/java/com/blinnnk/base/HoneyBaseAdapter.kt
@@ -0,0 +1,26 @@
+package com.blinnnk.base
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+@Suppress("UNCHECKED_CAST")
+abstract class HoneyBaseAdapter<DataType, R: View>
+  : RecyclerView.Adapter<HoneyBaseAdapter<DataType, R>.ViewHolder>() {
+
+  abstract val dataSet: ArrayList<DataType>
+  abstract fun generateCell(context: Context): R
+  abstract fun R.bindCell(data: DataType)
+
+  override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int) =
+    ViewHolder(generateCell(parent?.context!!))
+
+  override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    (holder?.itemView as? R)?.apply { bindCell(dataSet[position]) }
+  }
+
+  override fun getItemCount(): Int = dataSet.size
+
+  inner class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+}
